ed28d3050a8a17e57757a9d0739506cdce1a846f
nightwing <amirjanyan@gmail.com>
2014-11-17 10:58:29 +0000

    use old vim commandline for :

--- vim/keymap.js
+++ vim/keymap.js
@@ -3,4 +3,12 @@ define(function(require, exports, module) {
 exports.showCli = true;
 exports.aceKeyboardHandler = require("ace/keyboard/vim").handler;
 
+exports.aceKeyboardHandler.defaultKeymap.unshift(
+    
+    { keys: ':', type: 'action', action: 'aceCommand', actionArgs: { exec: function(ace) {
+        ace.showCommandLine(":");
+    } } }
+    
+);
+
 });
